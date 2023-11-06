95dd170c0e87c433393aef9976e18f5c134239f1
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2023-11-06 12:42:23 +0530

    stage 3

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import { addUser, forgotPassword, generateToken, getUser, getUserByID, resetPassword, generateActivationToken, activateAccount, generateUserToken, activationMail}from "../Controllers/users";
+import { addUser, forgotPassword, generateToken, getUser, getUserByID, resetPassword, generateActivationToken, activateAccount, generateUserToken, activationMail}from "../Controllers/users.js";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 
